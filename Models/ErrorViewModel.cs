9242cfc038459d8e450039b287ae3bd600b51ccb
Ludovico <ludovico.buffon@gmail.com>
2021-01-24 12:47:53 +0100

    test

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace pipelines_dotnet_core.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        public string Test { get; set; }           
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
